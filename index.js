8a75c32e941c6be367160918ac33abf1c3615d30
Adam Weaver <adamweaver90@gmail.com>
2022-09-29 13:38:11 -0400

    commit tree f009a546658ef0dd2c1918752801e50885f8ba67

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+//Use `if` and `else if` statements to return the
+//correct greeting based on the distance the passenger desires to travel.
+function scuberGreetingForFeet(distance){
+  if (distance < 400) {
+    console.log('This one is on me!')
+    'This one is on me!'
+  } 
 }
 
+//Use a ternary operator to return the correct response
 function ternaryCheckCity(){
   // Write your code here!
 }
 
+
+//Use a `switch` statement to return a different
 function switchOnCharmFromTip(){
   // Write your code here!
 }
